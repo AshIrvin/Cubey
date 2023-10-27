e1836c9319df583ecbbe190af0d61833c2d97442
Ash Irvin <AshIrvin@Outlook.com>
2023-10-27 17:30:10 +0100

    new UI script
    Most of the UI code in GameManager will get moved here

--- Assets/Game/Scripts/Managers/UiManager.cs
+++ Assets/Game/Scripts/Managers/UiManager.cs
@@ -1,92 +1,39 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class UiManager : MonoBehaviour
 {
-    // public static UiManager Instance { get; set; }
-    // No longer used?
-    
-    // old
-    [SerializeField] private Toggle debugConsoleToggle;
-    [SerializeField] private GameObject debugConsole;
-    [SerializeField] private GameObject tutorial;
-    [SerializeField] private GameObject tutorialTitleImage; 
-    [SerializeField] private GameObject tutorialSketchImage;
-    [SerializeField] private GameObject tutorialBonusSketch;
-    [SerializeField] private GameObject tutorialPause;
+    public static UiManager Instance;
 
-    [SerializeField] private Text tutorialText;
-    [SerializeField] private Text titleText;
+    [SerializeField] private List<string> finishedInfoText;
+    [SerializeField] private List<string> nearlyFinishedInfoText;
+    [SerializeField] private List<string> failedFinishedInfoText;
 
-    [SerializeField] private float tutorialTimer = 4f;
-    [SerializeField] private bool timerEnabled;
-    
-    public static GameObject continueButton;
+    private TextMeshProUGUI endScreenText;
+
 
     private void Awake()
     {
-        // Instance = this;
+        if (Instance == null)
+            Instance = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void UpdateEndScreenInfoText(GameManager.FinishedInfo info)
     {
-        //CloseScreens();
-        if (debugConsole == null)
-            debugConsole = GameObject.Find("IngameDebugConsole");
-        tutorial.SetActive(false);
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        tutorialTimer -= Time.fixedDeltaTime;
-        if (tutorialTimer < 0 && timerEnabled)
+        
+        switch (info)
         {
-            CloseScreens();
-            timerEnabled = false;
-            tutorialTimer = 4f;
+            case GameManager.FinishedInfo.Failed:
+                endScreenText.text = failedFinishedInfoText[UnityEngine.Random.Range(0, failedFinishedInfoText.Count)];
+                break;
+            case GameManager.FinishedInfo.Nearly:
+                endScreenText.text = nearlyFinishedInfoText[UnityEngine.Random.Range(0, nearlyFinishedInfoText.Count)];
+                break;
+            case GameManager.FinishedInfo.Completed:
+                endScreenText.text = finishedInfoText[UnityEngine.Random.Range(0, finishedInfoText.Count)];
+                break;
         }
     }
-
-    public void DebugConsole(bool on)
-    {
-        //GameObject console = GameObject.Find("IngameDebugConsole");
-        debugConsole.SetActive(on);
-    }
-
-    public void TutorialScreen(bool on)
-    {
-        Tutorial(on, "", "");
-    }
-
-    private void Tutorial(bool on, string title, string text)
-    {
-        CloseScreens();
-        tutorial.SetActive(on);
-        timerEnabled = true;
-        titleText.text = title;
-        tutorialText.text = text;
-        Time.timeScale = on ? 0f : 1f;
-    }
-
-    public void TutorialPause(bool on)
-    {
-        timerEnabled = true;
-        tutorialPause.SetActive(on);
-    }
-
-
-    private void CloseScreens()
-    {
-        //print("close tut screens");
-        tutorial.SetActive(false);
-        tutorialTitleImage.SetActive(false);
-        tutorialSketchImage.SetActive(false);
-        tutorialPause.SetActive(false);
-    }
-
 }
