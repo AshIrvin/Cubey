30e8a4bcc55d9f4759593ccc4648d9bd6a24b5eb
Ash Irvin <AshIrvin@Outlook.com>
2023-10-27 17:27:35 +0100

    Global script for scriptable objects
    A place to store chapter and level metadata

--- Assets/Game/Scripts/Framework/GlobalMetaData.cs
+++ Assets/Game/Scripts/Framework/GlobalMetaData.cs
@@ -1,8 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[DefaultExecutionOrder(0)]
 public class GlobalMetaData : MonoBehaviour
 {
-    public static ChapterList ChapterList;
+    public static GlobalMetaData Instance;
+
+    public ChapterList ChapterList;
+    private LevelMetaData levelMetaData;
+
+    public LevelMetaData LevelMetaData => levelMetaData;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    public void AssignLevelMetaData()
+    {
+        levelMetaData = ChapterList[SaveLoadManager.LastChapterPlayed].LevelList[SaveLoadManager.LastLevelPlayed];
+    }
 }
